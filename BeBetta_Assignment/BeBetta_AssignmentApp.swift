f858a96853970d4a3150df1ec5aeb2c77dd25448
Usha Mayuri <ushamayurim@gmail.com>
2024-01-26 14:10:56 +0530

    Initial Commit

---
+++ BeBetta_Assignment/BeBetta_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeBetta_AssignmentApp.swift
+//  BeBetta_Assignment
+//
+//  Created by usha mayuri on 26/01/24.
+//
+
+import SwiftUI
+
+@main
+struct BeBetta_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
